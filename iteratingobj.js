d4faa0f73234e8768fe74fa69d735491ab636b70
Evan Vogts <evanjvogts@gmail.com>
2019-09-18 13:36:25 -0500

    enroll in summer school

--- iteratingobj.js
+++ iteratingobj.js
@@ -12,3 +12,27 @@ studentData = [
 }
 ]
 
+
+//Enrolling in summer school
+function enrollInSummerSchool (students) {
+    return data.map (function (obj) {
+        return `${obj.name}, status: 'In Summer School', ${obj.course}`;
+}
+
+studentData2 = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
