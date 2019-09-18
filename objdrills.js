d5eed50ae183b8bbbe25e253e3cddeca0540538e
Evan Vogts <evanjvogts@gmail.com>
2019-09-18 15:09:28 -0500

    iterating over an objects properties

--- objdrills.js
+++ objdrills.js
@@ -10,3 +10,16 @@ loaf.hydration = loaf.flour/loaf.water*100;
 
 console.log(loaf);
 
+
+//Iterating over an objects props
+const obj = {
+  foo: 2,
+  bar: 'cat',
+  fum: 'banana',
+  quux: 'bicycle',
+  spam: 'hawaii',
+}
+
+for (const key in obj) {
+  console.log(obj);
+}
