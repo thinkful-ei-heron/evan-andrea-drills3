a856d1ac2913e753cdeb388c7d015c02f0edc22e
Evan Vogts <evanjvogts@gmail.com>
2019-09-18 15:42:09 -0500

    Arrays of objects

--- objdrills.js
+++ objdrills.js
@@ -29,3 +29,19 @@ const obj2 = {
   meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 }
  console.log(obj2.meals[3]);
+
+
+ //Arrays of objects
+ const obj3 = [
+  {name: 'Jane', jobTitle: 'Accountant'},
+  {name: 'Bob', jobTitle: 'Firefighter'},
+  {name: 'Jim', jobTitle: 'Sales'}
+]
+
+let allPeople = [];
+
+for(let key in obj3) {
+  allPeople.push(obj3[key])
+}
+
+console.log(...allPeople);
